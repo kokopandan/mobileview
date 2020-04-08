3d6c2871b2fa66ebf41ab58b6540c01fc05f8e31
kokojuned <ashritiko78@gmail.com>
2020-04-08 16:08:23 +0700

    Mobile Version

---
+++ assets/css/js.js
@@ -0,0 +1,15 @@
+
+$("#close-side").on('click' , function(){
+
+    $(".side-menu-container").hide();
+
+});
+
+
+
+
+$(".navbar-toggler").on('click' , function(){
+
+    $(".side-menu-container").show()
+
+});
